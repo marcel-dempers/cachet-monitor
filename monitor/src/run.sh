a48ce9ef47438126eae126dddff3c8902ef6db52
marcel-dempers <aimvec@gmail.com>
2018-01-29 13:57:02 +1100

    start a local monitor and link to sample hello container

---
+++ monitor/src/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export MSYS_NO_PATHCONV=1
+
+docker build . -t cachet-monitor
+
+#docker rm --force cachet-monitor
+echo "starting monitor..."
+docker run -it --rm --link google-hello --link cachet \
+-e CACHET_SERVER_URL=http://cachet:8000/api/v1 \
+-e CACHET_TOKEN=zlvNVV0VxKkuRL0WM5ww \
+-v $PWD/config:/app/Configs cachet-monitor
